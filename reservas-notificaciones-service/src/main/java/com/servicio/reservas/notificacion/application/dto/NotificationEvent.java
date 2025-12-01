8d32290ddd32fb7a530cb25641ab6d89e4cf5ba2
Samuel <escobar.samuel@correounivalle.edu.co>
2025-11-30 23:08:38 -0500

    Se realiza la configuracion del email y los templates

---
+++ reservas-notificaciones-service/src/main/java/com/servicio/reservas/notificacion/application/dto/NotificationEvent.java
@@ -0,0 +1,21 @@
+package com.servicio.reservas.notificacion.application.dto;
+
+import com.servicio.reservas.notificacion.domain.entities.NotificationType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class NotificationEvent implements Serializable {
+    private String recipientEmail;
+    private NotificationType notificationType;
+    private Map<String, Object> templateModel; // Data for the email template
+    private LocalDateTime timestamp;
+}
