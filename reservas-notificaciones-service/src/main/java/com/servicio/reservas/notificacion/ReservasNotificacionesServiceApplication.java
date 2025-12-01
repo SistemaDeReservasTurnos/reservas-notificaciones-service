e27eca372f9e7b7c135f2b937acae0177014b7ea
Samuel <escobar.samuel@correounivalle.edu.co>
2025-11-30 21:33:28 -0500

    Se crea la estructura base del proyecto

---
+++ reservas-notificaciones-service/src/main/java/com/servicio/reservas/notificacion/ReservasNotificacionesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.servicio.reservas.notificacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservasNotificacionesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservasNotificacionesServiceApplication.class, args);
+	}
+
+}
---
+++ reservas-notificaciones-service/src/test/java/com/servicio/reservas/notificacion/ReservasNotificacionesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servicio.reservas.notificacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservasNotificacionesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
